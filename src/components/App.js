7fa593c7e623c7286f63650113b5af3968a99a26
mj305 <maria.j.beckles@gmail.com>
2020-08-17 17:12:32 -0400

    added styled-components, jsonconfig, and button component

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Button from 'components/common/Button';
+
+
+function App() {
+  return (
+    <>
+      <h1>APP</h1>
+      <Button>
+        Submit
+      </Button>
+    </>
+  );
+}
+
+export default App;
